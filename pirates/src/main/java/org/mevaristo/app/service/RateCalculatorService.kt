4e5e32d9eb65227ded9c603bd919bba1c43844fc
mevaristo <matheus.evaristo@protonmail.com>
2024-09-28 23:29:47 -0300

    Define, implement and test premises for rate calculation

--- pirates/src/main/java/org/mevaristo/app/service/RateCalculatorService.kt
+++ pirates/src/main/java/org/mevaristo/app/service/RateCalculatorService.kt
@@ -2,6 +2,9 @@ package org.mevaristo.app.service
 
 import org.mevaristo.app.model.PricePoint
 import org.mevaristo.app.model.Rate
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
+import kotlin.math.abs
 
 class RateCalculatorService {
     fun calculate(subject: Rate, rateAgainst: Rate): Rate {
@@ -15,14 +18,21 @@ class RateCalculatorService {
             ratedPrices,
         )
     }
-
+    
     private fun calculatePricePoint(pricePoint: PricePoint, rateAgainst: Rate): PricePoint {
+        var closestMatchBelow: PricePoint = PricePoint(1.0, LocalDate.MIN)
         rateAgainst.pricePoints.map { againstPricePoint ->
-            if (againstPricePoint.date.isAfter(pricePoint.date)) {
+            if (pricePoint.date.isEqual(againstPricePoint.date)) {
                 return PricePoint(pricePoint.price / againstPricePoint.price, pricePoint.date)
+            }
+
+            if (pricePoint.date.isAfter(againstPricePoint.date)) {
+                if (abs(ChronoUnit.DAYS.between(pricePoint.date, againstPricePoint.date)) <
+                    abs(ChronoUnit.DAYS.between(pricePoint.date, closestMatchBelow.date)))
+                closestMatchBelow = againstPricePoint
             }
         }
 
-        return PricePoint(pricePoint.price / rateAgainst.pricePoints.last().price, pricePoint.date)
+        return PricePoint(pricePoint.price / closestMatchBelow.price, pricePoint.date)
     }
 }
